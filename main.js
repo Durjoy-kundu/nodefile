9ac3cc5aa9c7d73b3b27caf5b14c8c12dcea6561
Durjoy kundu <durjoykundu07@gmail.com>
2025-01-18 23:08:57 +0600

    callback hell , append file

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ console.log("starting");
 // Correctly writing to a file using `writeFileSync`
 //fs.writeFileSync("harry.txt", "harry is a good boy");
 fs.writeFile("harry2.txt", "harry is not a bad boy", ()=>{
-    console.log("done");
-    fs.readFile("harry2.txt",(error,data)=>{
-        console.log(error,data);
-    }
+    console.log("done")
+    fs.readFile("harry2.txt", (error,data)=>{
+        console.log(error,data.toString());
+
+    })
 });
+
+// appending to the file
+fs.appendFile("harry2.txt", "harryrobbo", (e,d)=>{
+    console.log(d)
+})
 
 console.log(" the happy ending");
 
