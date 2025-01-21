a5564ea51c702e38baf05f3635a255b5620cde26
Durjoy kundu <durjoykundu07@gmail.com>
2025-01-21 23:43:43 +0600

    werfe

--- aboutPath.js
+++ aboutPath.js
@@ -1,5 +1,7 @@
 import path from "path"
 let mypath = "F:\\web develop\\Nodejs all\\nodefile\\harry.txt"
 console.log(path.extname(mypath))
+console.log(path.dirname(mypath))
+console.log(path.basename(mypath))
 
 //console.log(path)
